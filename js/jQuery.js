78dfce73f91e356c19a16f085d877dd6d8be21f8
Kasper Steensig <kasp8345@edu.campusvejle.dk>
2016-01-04 11:36:09 +0100

    masse nyt til siden
    menuen er opsat samt et par billeder er indsat i header som et
    slideshow..

---
+++ js/jQuery.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+   
+    $('<div class="carousel-caption"><h1>JG Isenkram</h1><h2>Isenkram<br>Storkøkken<br>Fast-food udstyr</h2></div>').insertAfter('#headertext div img')
+    
+});
+
